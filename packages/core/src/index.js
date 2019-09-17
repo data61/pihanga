0ba4020f6105b276fe29b69fdd34119e67927c93
Max <max.ott@data61.csiro.au>
2019-09-17 15:03:10 +1000

    core: forgot to export new rest client

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -3,6 +3,13 @@ export * from './logger/index';
 export * from './redux';
 export * from './card.service';
 export * from './start';
+
+// ignoring 'init' export
+// eslint-disable-next-line import/export
 export * from './router';
+
 export * from './utils';
 export * from './backend';
+// ignoring 'init' export
+// eslint-disable-next-line import/export
+export * from './rest_client';
